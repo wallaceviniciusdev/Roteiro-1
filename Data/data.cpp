0fab304a6e2d32d0efb2b6ffc70cf1a83e4973e4
Wallace Vinicius <45434649+wallaceviniciusdev@users.noreply.github.com>
2019-02-20 15:48:47 -0300

    Update data.cpp

--- Data/data.cpp
+++ Data/data.cpp
@@ -8,54 +8,56 @@ Data::Data(){
 }
 
 void Data::setDia(int dia){
-	this->dia = dia;
+	if(this->dia <= 31){
+		this->dia = dia;
+	}else{
+		cout << "Dia invalido" << endl;
+	}
 }
 
 void Data::setMes(int mes){
-	this->mes = mes;
+	if(this->mes <= 12 && this>mes >= 1){
+		this->mes = mes;
+	}else{
+		cout << "Mês invalido" << endl;
+	}
 }
 
 void Data::setAno(int ano){
-	this->ano = ano;
+	if(this->ano < 0){
+		this->ano = ano;
+	}
 }
 
 void Data::avancarData(){
-	
-}
-
-void Data::avancarDataIndice(int indice){
-	if(this->mes==4 || this->mes==6 || this->mes==4||this->mes==9||this->mes==11){
-		if(this->dia!=30){
-			cout<< this->dia+1 <<" / "<<this->mes<<" / "<<this->ano<<"\n";
-		}
-		else if(dia==31){
+	if(this->mes==4 || this->mes==6 || this->mes==9 || this->mes==11){
+		if(this->dia != 30){
+			cout << this->dia+1 <<" / "<< this->mes << " / " <<this->ano << endl;
+		}else if(this->dia >= 31){
 			cout<<"Data Inexistente!\n";
+		}else{
+			this->dia = 1;
+			cout<< this->dia << "/ " << this->mes+1 << " / " << this->ano <<endl";
 		}
-		else{
-			cout<<"01 / "<<this->mes+1<< " / "<<this->ano<<"\n";
-		}
-	}
-	else if(this->mes==2){
-
+	}else if(this->mes==2){
 		if(this->dia!=28 && this->dia !=29){
-			cout<<this->dia+1<<" / 02 / "<<this->ano<<" \n";
-		}
-		else{
+			cout<<this->dia+1<<" / " << this->mes << " / " <<this->ano<<" \n";
+		}else{
 			if(this->mes==2 && this->ano%4==0){
 				if(this->dia==29){
-					cout<<"01 / 03 / "<<this->ano<<"\n";
-				}
-				else{
-					cout<<"29 / 02 / "<<this->ano<<"\n";
+					this->dia = 1;
+					cout<<"01 / " << this->mes+1 << " / "<<this->ano<<"\n";
+				}else{
+					cout<< this->dia+1 << "/" << this->mes << "/" << this->ano <<"\n";
 				}
 			}
 
 		}
-	}
-	else if(this->mes==12 && this->dia==31){
-		cout<<"01 / 01 / "<<this->ano+1<<"\n";
-	}
-	else{
+	}else if(this->mes==12 && this->dia==31){
+		this->dia = 1;
+		this->mes = 1;
+		cout << this->dia << "/" << this->mes << "/" <<this->ano+1<<"\n";
+	}else{
 		cout<<dia+1 <<" / "<<mes<<" / "<<ano<<"\n";
 	}
 }
